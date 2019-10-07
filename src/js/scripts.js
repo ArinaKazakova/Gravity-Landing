2128b8b0fc1d54a26eadb63b385f6644f3ad3651
ArinaKazakova <arinakazakovak@gmail.com>
2019-10-07 14:48:08 +0300

    create services section, add img for slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,14 +2,44 @@ $(document).ready(function() {
     $('.navbar-search__field').hide();
     $('.navbar-search__toggle').on('click', function () {
         $('.navbar-search__field').animate({height:'toggle',
-        opacity: 'toggle'}, 600);
+            opacity: 'toggle'}, 500);
     });
 
 
     $('.menu__wrap').hide();
     $('.menu__block').on('click', function () {
-            $('.menu__wrap').slideToggle();
-        }
+        $('.menu__wrap').slideToggle();
+    }
     );
-    document.addEventListener('click', function(){ console.log(this, arguments); });
+
+    let width = $('.services-slider__item').outerWidth(true);
+    let slideCount = $('.services-slider__container').children().length;
+    let slideLeft = width + (width/2);
+    let sliderWidth = slideCount  * width;
+    let slideNow = 1;
+    $('.services-slider__container').css('width', sliderWidth);
+    
+
+
+    $('.services-slider__item').eq(slideNow ).css('opacity', '0.5');
+    
+    console.log(slideNow);
+    $('#next').on('click', function nextSlide() {
+        $('.services-slider__item').eq(slideNow + 1).css('opacity', '0.5');
+        $('.services-slider__item').eq(slideNow).css('opacity', '1');
+        console.log(slideNow);
+        if (slideNow == slideCount || slideNow <= 0 ) {
+            $('.services-slider__container').css('transform', 'translate(0, 0)');
+            slideNow = 1;
+            $('.services-slider__item').eq(slideNow ).css('opacity', '0.5');
+        } else {
+            translateWidth = - width * (slideNow);
+            $('.services-slider__container').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow++;
+        }
+    })
 });
